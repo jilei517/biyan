471b4ce83530bf2fb21ccd8345d1a53434f4c7a3
Admin密码0000 <Admin@192.168.1.218>
2026-09-22 17:56:09 +0800

    1.0.2

--- android/app/src/main/kotlin/com/example/biyan/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.biyan
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/models/hobby_category.dart
+++
@@ -1,9 +0,0 @@
-class HobbyCategory {
-  const HobbyCategory({
-    required this.title,
-    required this.tags,
-  });
-
-  final String title;
-  final List<String> tags;
-}
